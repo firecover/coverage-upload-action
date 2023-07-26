2744b3be8b32b297f39c55faa5b5067db37f28dd
Vajahath Ahmed <vajuoff.1@gmail.com>
2023-07-27 00:01:53 +0530

    adding debug logs

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,7 @@ import { readFile } from "node:fs/promises";
 import YAML from "yaml";
 import { join } from "node:path";
 import { cwd } from "node:process";
+import { Logger } from "./logger";
 
 export const defaultConfig: FirecoverYML = {
   components: [{ componentId: "default", name: "default", paths: ["**"] }],
@@ -11,15 +12,34 @@ export const defaultConfig: FirecoverYML = {
 @injectable()
 @singleton()
 export class Config {
+  constructor(private readonly logger: Logger) {}
+
   getSignedRequestEndpoint(): string {
+    this.logger.debug(`[config/getSignedRequestEndpoint] => todo`);
     return "todo";
   }
 
   async getRepoFirecoverYmlSettings(): Promise<FirecoverYML> {
     try {
-      const firecoverYml = await readFile(join(cwd(), "./firecover.yml"));
+      const configFileLocation = join(cwd(), "./firecover.yml");
+      this.logger.debug(
+        `[config/getRepoFirecoverYmlSettings] looking for ${configFileLocation}`
+      );
+      const firecoverYml = await readFile(configFileLocation);
+      this.logger.debug(
+        `[config/getRepoFirecoverYmlSettings] found ${configFileLocation}, yaml parsing...`
+      );
       const firecoverConfig = YAML.parse(firecoverYml.toString()) || {};
-      return { ...defaultConfig, ...firecoverConfig };
+      this.logger.debug(
+        `[config/getRepoFirecoverYmlSettings] found ${configFileLocation}, yaml parsing...`
+      );
+      const mergedConfig = { ...defaultConfig, ...firecoverConfig };
+      this.logger.debug(
+        `[config/getRepoFirecoverYmlSettings] mergedConfig ${JSON.stringify(
+          mergedConfig
+        )}`
+      );
+      return mergedConfig;
     } catch {
       return defaultConfig;
     }
@@ -27,7 +47,13 @@ export class Config {
 
   async getComponentList(): Promise<FirecoverYML["components"]> {
     const settings = await this.getRepoFirecoverYmlSettings();
-    return settings.components;
+    const components = settings.components;
+    this.logger.debug(
+      `[config/getComponentList] components identified: ${JSON.stringify(
+        components
+      )}`
+    );
+    return components;
   }
 }
 
