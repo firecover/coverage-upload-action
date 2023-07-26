02d12cd09e2cc3be9b4e974645f32bdb70cb771d
Vajahath Ahmed <vajuoff.1@gmail.com>
2023-07-27 03:40:02 +0530

    updating debug logs

--- src/utils/coverage-finder.ts
+++ src/utils/coverage-finder.ts
@@ -137,9 +137,9 @@ export class CoverageFinder {
         for (const type of Object.keys(
           summary[file],
         ) as unknown as (keyof FullCoverage)[]) {
-          // this.logger.log(
-          //   JSON.stringify({ file, type, summary, summaryAggregation }),
-          // );
+          this.logger.log(
+            JSON.stringify({ file, type, summary, summaryAggregation }),
+          );
           summaryAggregation[file][type].covered += summary[file][type].covered;
           summaryAggregation[file][type].skipped += summary[file][type].skipped;
           summaryAggregation[file][type].total += summary[file][type].total;
